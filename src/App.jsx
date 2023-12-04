b5d296fa7a97daf5aa45e9c02ddc1e8a4866e920
Manoj <manojgowdacs35@gmail.com>
2023-12-04 11:25:28 +0530

    Basic react project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ErrorExample from "../tutorial/1-useState/1-error-example";
+
+function App(){
+  return (
+    <div className="container">
+      <ErrorExample />
+    </div>
+  );
+}
+
+export default App;
