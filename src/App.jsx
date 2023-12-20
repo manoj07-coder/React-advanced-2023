a0bea308c960395f1b6ba4390609c106a008a39b
Manoj <manojgowdacs35@gmail.com>
2023-12-20 17:56:11 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import React from "react";
-import Index from "../tutorial/12-memo-usecallback-usememo";
+import React from 'react'
+import { createBrowserRouter,createRoutesFromElements,Route,RouterProvider, Routes } from 'react-router-dom'
+import Home from '../tutorial/11-react-router/Home'
+import About from '../tutorial/11-react-router/About'
+import Nav from '../tutorial/11-react-router/Nav'
+import People from '../tutorial/11-react-router/People'
+import Error from '../tutorial/11-react-router/Error'
 
-function App(){
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Nav />} >
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/people' element={<People />} />
+    </Route>
+  )
+
+)
+
+const App = () => {
   return (
-    <div className="container">
-      <Index />
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
 
-export default App;
+export default App
