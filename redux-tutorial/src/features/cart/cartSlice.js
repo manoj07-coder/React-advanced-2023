637e752295fd96d0440f531f0753a933d4fc896c
Manoj <manojgowdacs35@gmail.com>
2024-01-16 23:14:05 +0530

    cart slice file

--- redux-tutorial/src/features/cart/cartSlice.js
+++ redux-tutorial/src/features/cart/cartSlice.js
@@ -1,17 +1,47 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import cartItems from '../../cartItems';
 
-const intialState = {
-    cartItems : [],
-    amount : 0,
-    total : 0,
-    isLoading : true,
+const initialState = {
+    cartItems: cartItems,
+    amount: 4,
+    total: 0,
+    isLoading: true,
 }
 
 const cartSlice = createSlice({
-    name:'cart',
-    intialState,
+    name: 'cart',
+    initialState,
+    reducers: {
+        clearCart: (state) => {
+            state.cartItems = []
+        },
+        removeItem: (state, action) => {
+            const itemId = action.payload;
+            state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
+        },
+        increase: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload);
+            cartItem.amount = cartItem.amount + 1;
+        },
+        decrease: (state, { payload }) => {
+            const cartItem = state.cartItems.find((item) => item.id === payload);
+            cartItem.amount = cartItem.amount - 1;
+        },
+        calculateTotal:(state)=>{
+            let amount = 0;
+            let total = 0;
+            state.cartItems.forEach((item)=>{
+                amount += item.amount;
+                total += item.amount * item.price;
+            })
+            state.amount = amount;
+            state.total = total;
+        }
+    }
 })
 
-// console.log(cartSlice);
+export const { clearCart, removeItem, increase, decrease,calculateTotal } = cartSlice.actions;
+
+console.log(cartSlice);
 
 export default cartSlice.reducer;
