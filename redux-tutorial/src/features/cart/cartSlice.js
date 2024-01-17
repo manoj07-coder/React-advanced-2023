81bb30089534c6bf29eb3c6876e7b7d670bdab98
Manoj <manojgowdacs35@gmail.com>
2024-01-17 11:36:44 +0530

    added builder for extra reducers

--- redux-tutorial/src/features/cart/cartSlice.js
+++ redux-tutorial/src/features/cart/cartSlice.js
@@ -1,12 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
 import cartItems from '../../cartItems';
+import axios from 'axios'
+
+const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-    cartItems: cartItems,
-    amount: 4,
+    cartItems: [],
+    amount: 0,
     total: 0,
     isLoading: true,
 }
+
+export const getCartItems = createAsyncThunk('cart/getCartItems',()=>{
+   return fetch(url)
+   .then((res)=>res.json())
+   .catch((err)=>console.log(err))
+}
+)
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -37,11 +47,24 @@ const cartSlice = createSlice({
             state.amount = amount;
             state.total = total;
         }
+    },
+    extraReducers: (builder)=>{
+       builder
+       .addCase(getCartItems.pending,(state)=>{
+        state.isLoading = true;
+       })
+       .addCase(getCartItems.fulfilled,(state,action)=>{
+        state.isLoading = false;
+        state.cartItems = action.payload;
+       })
+       .addCase(getCartItems.rejected,(state)=>{
+        state.isLoading = false;
+       })
     }
 })
 
 export const { clearCart, removeItem, increase, decrease,calculateTotal } = cartSlice.actions;
 
-console.log(cartSlice);
+// console.log(cartSlice);
 
 export default cartSlice.reducer;
