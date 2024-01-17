4132db3178d1d38cea1e37ee4696c555f7448096
Manoj <manojgowdacs35@gmail.com>
2024-01-17 11:37:04 +0530

    bug fixes

--- redux-tutorial/src/App.jsx
+++ redux-tutorial/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import CartContainer from './components/cartContainer'
 import { useDispatch,useSelector } from 'react-redux'
-import { calculateTotal } from './features/cart/cartSlice.js'
+import { calculateTotal, getCartItems } from './features/cart/cartSlice.js'
 import Modal from './components/Modal.jsx'
 
 
@@ -10,17 +10,28 @@ const App = () => {
 
   const dispatch = useDispatch();
 
-  const {cartItems} = useSelector((store)=>store.cart);
+  const {cartItems,isLoading} = useSelector((store)=>store.cart);
   const {isOpen} = useSelector((store)=>store.modal)
 
   useEffect(()=>{
     dispatch(calculateTotal())
   },[cartItems]);
 
+  useEffect(()=>{
+    dispatch(getCartItems())
+  },[])
+
+  if(isLoading){
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
   return (
     <main>
       {isOpen && <Modal /> }
-      
       <Navbar />
       <CartContainer />
     </main>
